b9dabb92add2b88d64a6af91f3368eb99804a8a8
Mark Hernandez <m.hern1415@gmail.com>
2019-09-09 23:17:27 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,11 +1,7 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
-
-
-def change
-  
-  
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
-
-
-end
-
